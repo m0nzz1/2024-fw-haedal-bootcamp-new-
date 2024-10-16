ce00d7c1cf81951512c32ba5a73ed7e274809e1a
Jominji <whaldzii@gmail.com>
2024-10-16 23:55:08 +0900

    별 찍기 - 2

---
+++ 백준2439.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - 1; i <= j; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
